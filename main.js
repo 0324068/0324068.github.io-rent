591d9b2e4000acc1ac7501b875af7894a2f1cae9
alan <alanlai1221@gmail.com>
2019-05-12 15:54:44 +0800

    5/12

--- main.js
+++ main.js
@@ -148,5 +148,20 @@ $(document).ready(function () {
         title:'B棟'
     });
       })
-   
+    
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId            : '318854485453171',
+          autoLogAppEvents : true,
+          xfbml            : true,
+          version          : 'v2.11'
+        });
+      };
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));  
 })    
